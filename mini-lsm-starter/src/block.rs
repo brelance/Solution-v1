e04da6ec0a32469a099c4a47d1597766e8116ef8
brelance <brelance82@gmail.com>
2024-01-14 18:26:05 +0800

    Day1: builder and block module

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -4,9 +4,11 @@
 mod builder;
 mod iterator;
 
+use std::{io::Read, iter, ops::Index};
+
 pub use builder::BlockBuilder;
 /// You may want to check `bytes::BufMut` out when manipulating continuous chunks of memory
-use bytes::Bytes;
+use bytes::{Bytes, Buf, BufMut};
 pub use iterator::BlockIterator;
 
 /// A block is the smallest unit of read and caching in LSM tree.
@@ -18,6 +20,9 @@ pub use iterator::BlockIterator;
 /// ----------------------------------------------------------------------------------------------------
 /// | Entry #1 | Entry #2 | ... | Entry #N | Offset #1 | Offset #2 | ... | Offset #N | num_of_elements |
 /// ----------------------------------------------------------------------------------------------------
+
+const BLOCK_SIZE: usize =  4096;
+
 pub struct Block {
     data: Vec<u8>,
     offsets: Vec<u16>,
@@ -27,14 +32,76 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the tutorial
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
-    }
+        let mut encoded = self.data.clone();
+        for &offset in self.offsets.iter() {
+            encoded.put_u16(offset);
+        }
 
-    /// Decode from the data layout, transform the input `data` to a single `Block`
+        encoded.put_u16(self.offsets.len() as u16);
+        Bytes::from(encoded)
+    }
+        
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let src_len = data.len();
+        let num_of_elements = (data[src_len - 2] as u16 ) << 8 | data[src_len - 1] as u16;
+        let offset_start_pos = src_len - 2 - num_of_elements as usize * 2;
+
+        let kv_data = data[0..offset_start_pos].to_vec();
+        let offset_slice = &data[offset_start_pos..src_len - 2];
+        // let offsets: Vec<u16> = offset_slice
+        //     .chunks(2)
+        //     .into_iter()
+        //     .map(|high, low| (high as u16) << 8 | low as u16)
+        let mut offsets = Vec::new();
+        for pair in offset_slice.chunks(2) {
+            match pair {
+                &[high, low] => {
+                    let offset = (high as u16) << 8 | low as u16;
+                    offsets.push(offset);
+                }
+                _ => {}
+            }
+        }
+        
+        Block {data: kv_data, offsets} 
+        // let block_builder: BlockBuilder = BlockBuilder::new(BLOCK_SIZE);
     }
 }
 
 #[cfg(test)]
 mod tests;
+
+#[cfg(test)]
+mod test {
+    use crate::block::{BlockBuilder, Block};
+    #[test]
+    fn test_chunk() {
+        let numbers: &[u8] = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        // let offsets: Vec<u16> = numbers
+        //     .chunks(2)
+        //     .into_iter()
+        //     .map(|&[high, low]| (high as u16) << 8 | low as u16 )
+        //     .collect();
+       
+        for pair in numbers.chunks(2) {
+            match pair {
+                &[a, b] => println!("Pair: {}, {}", a, b),
+                &[a] => println!("Single element: {}", a),
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    #[test]
+    fn test_encode_decode() {
+        let mut builder = BlockBuilder::new(16);
+        builder.add(b"233", b"233333");
+        builder.add(b"122", b"122222");
+        let mut block =  builder.build();
+        let data = block.encode();
+        Block::decode(&data);
+    }
+}
+
+
--- mini-lsm-starter/src/block/iterator.rs
+++ mini-lsm-starter/src/block/iterator.rs
@@ -1,7 +1,9 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use std::sync::Arc;
+use std::{sync::Arc, borrow::Borrow, ops::Index};
+
+use crate::iterators::StorageIterator;
 
 use super::Block;
 
@@ -29,22 +31,35 @@ impl BlockIterator {
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let (key, value) = BlockIterator::seek_key_value_within_offset(block.clone(), 0);
+        BlockIterator { 
+            block, 
+            key, 
+            value,
+            idx: 0, 
+        }
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: &[u8]) -> Self {
-        unimplemented!()
+        let (key, value, _) = BlockIterator::seek_key(block.clone(), key);
+        
+        BlockIterator { 
+            block, 
+            key,
+            value,
+            idx: 0, 
+        }
     }
 
     /// Returns the key of the current entry.
     pub fn key(&self) -> &[u8] {
-        unimplemented!()
+        &self.key
     }
 
     /// Returns the value of the current entry.
     pub fn value(&self) -> &[u8] {
-        unimplemented!()
+        &self.value
     }
 
     /// Returns true if the iterator is valid.
@@ -55,17 +70,76 @@ impl BlockIterator {
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        unimplemented!()
+        let (key, value) =
+            BlockIterator::seek_key_value_within_offset(self.block.clone(), 0);
+
+        self.key = key;
+        self.value = value;
+        self.idx = 0;
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        let (key, value) =
+            BlockIterator::seek_key_value_within_offset(self.block.clone(), self.idx + 1);
+        self.key = key;
+        self.value = value;
     }
 
     /// Seek to the first key that >= `key`.
     /// Note: You should assume the key-value pairs in the block are sorted when being added by callers.
     pub fn seek_to_key(&mut self, key: &[u8]) {
-        unimplemented!()
+        let (key, value, offset) = BlockIterator::seek_key(self.block.clone(), key);
+        self.key = key;
+        self.value = value;
+        self.idx = offset;
     }
+
+    fn seek_key(block: Arc<Block>, key: &[u8]) -> (Vec<u8>, Vec<u8>, usize) {
+        let mut left = 0;
+        let mut right = block.offsets.len() - 1;
+        let mut mid = 0;
+
+        while left <= right {
+            mid = (left + right) / 2;
+            let cur_key: Vec<u8> = BlockIterator::seek_key_within_offset(block.clone(), mid);
+
+            match cur_key.as_slice().cmp(key) {
+                std::cmp::Ordering::Less => {
+                    left = mid + 1;
+                }
+                std::cmp::Ordering::Greater => {
+                    right = mid - 1;
+                }
+                std::cmp::Ordering::Equal => {
+                    break;
+                }
+            }
+        }
+        let (key, value) = BlockIterator::seek_key_value_within_offset(block.clone(), mid);
+
+        (key, value, mid)
+    }
+
+    fn seek_key_within_offset(block: Arc<Block>, offset: usize) -> Vec<u8> {
+        let pos = u16::from_be_bytes([block.data[offset], block.data[offset + 1]]) as usize;
+        let key_len = u16::from_be_bytes([block.data[pos], block.data[pos + 1]]) as usize;
+
+        block.data[pos + 2..pos + 2 + key_len].to_vec()
+    }
+
+    fn seek_key_value_within_offset(block: Arc<Block>, offset: usize) -> (Vec<u8>, Vec<u8>) {
+        let pos = u16::from_be_bytes([block.data[offset], block.data[offset + 1]]) as usize;
+        let key_len = u16::from_be_bytes([block.data[pos], block.data[pos + 1]]) as usize;
+
+        let value_len_pos = pos + key_len + 2;
+        let value_len = u16::from_be_bytes([block.data[value_len_pos], block.data[value_len_pos + 1]]) as usize;
+        
+        let key = block.data[pos + 2..pos + 2 + key_len].to_vec();
+        let value_pos = pos + key_len + 4;
+        let value = block.data[value_pos..(value_pos + value_len)].to_vec();
+        (key, value)
+    }
+
+
 }
